b842bb633e4ef10bc90b81a09f203c117fcdcabd
kapukap <kirill.kapustin29@gmail.com>
2022-08-05 22:11:34 +0300

    Add Auth Guard Decoracator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,14 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {CreateTaskDto} from "./dto/create-task.dto";
 import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
 import {UpdateTaskStatusDto} from "./dto/update-task-status.dto";
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 // Only Endpoints, Communicating with services and return res
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
