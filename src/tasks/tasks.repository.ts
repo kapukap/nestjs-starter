f7ba8c64f1dbc7a7cfb511918729ba4601b02eff
kapukap <kirill.kapustin29@gmail.com>
2022-08-05 15:01:16 +0300

    Decompose some logic to repo

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,14 +4,30 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
-export class TasksRepository extends Repository<Task>{
+export class TasksRepository extends Repository<Task> {
     constructor(
         @InjectRepository(Task)
-        private tasksRepository: Repository<Task>
+        private tasksRepository: Repository<Task>,
     ) {
-        super(tasksRepository.target, tasksRepository.manager)
+        super(tasksRepository.target, tasksRepository.manager);
+    }
+
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const { status, search } = filterDto;
+        const query = this.createQueryBuilder('task');
+        if (status) {
+            query.andWhere('task.status = :status', { status: status });
+        }
+
+        if (search) {
+            query.andWhere(
+                'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+                { search: `%${search}%` }); // for looking combinations in diff part of words
+        }
+        return await query.getMany();
     }
 
     async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
